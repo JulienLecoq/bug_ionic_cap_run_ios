9d6799e3af4635f33c54086aa5e71296e70767d9
Julien Lecoq <julien_lecoq@protonmail.com>
2024-09-20 10:57:18 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bug_ionic_cap_run_ios',
+  webDir: 'www'
+};
+
+export default config;
